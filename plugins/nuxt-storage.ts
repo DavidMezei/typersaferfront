171758f73201cb4a82dc3679b0620e8b93ceefca
David Mezei <davidmezei@hotmail.com>
2023-10-13 22:21:36 +0300

    config2

---
+++ plugins/nuxt-storage.ts
@@ -0,0 +1,8 @@
+// plugins/nuxt-storage.ts
+
+import Vue from 'vue';
+import nuxtStorage from 'nuxt-storage';
+
+// Create a new instance of VueStorage and add it to Vue as a plugi
+
+Vue.use(nuxtStorage);
